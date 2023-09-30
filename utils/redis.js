b9938460e2cfcc26059aa44877fae385130e8b0e
Crysart <idukrystal@gmail.com>
2023-09-30 01:14:12 +0100

    tests

--- utils/redis.js
+++ utils/redis.js
@@ -1,4 +1,5 @@
 import { createClient } from 'redis';
+import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
@@ -7,4 +8,27 @@ class RedisClient {
                                      ))
       .connect();
   }
+
+  isAlive() {
+    if (this.client.isReady)
+      return true;
+    return false;
+  }
+  async get(key) {
+    const get = promisify(this.client.get).bind(this.client);
+    value = await get(key);
+    return value;
+  }
+  async set(key, value, duration) {
+    const set = promisify(this.client.set).bind(this.client);
+    await set(key, value);
+    await this.client.expire(key, duration);
+  }
+  async del(key) {
+    const del = promisify(this.client.set).bind(this.client);
+    await del(key);
+  }
 }
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
