b3aa8105c0f1803d54623a05b884b50cc024d756
Crysart <idukrystal@gmail.com>
2023-11-02 17:04:57 +0100

    retry 2

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,10 @@
-import { Router, json } from 'express';
-
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
-
 import UsersController from '../controllers/UsersController';
-
 import AuthController from '../controllers/AuthController';
-
 import FilesController from '../controllers/FilesController';
 
 const router = Router();
-
-router.use(json());
 
 router.get('/status', AppController.getStatus);
 
@@ -25,5 +19,15 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 
 router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
